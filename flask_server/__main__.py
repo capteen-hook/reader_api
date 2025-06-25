bbc8e80e94f4784e4a62fb6d20cec915fd3999fb
Tristan H <tristanskyhook@gmail.com>
2025-06-25 12:58:14 -0700

    read me

--- flask_server/__main__.py
+++ flask_server/__main__.py
@@ -15,13 +15,19 @@ import gc
 from flask_server.prompts import (fill_form, fill_home_form, fill_appliance_form, default_home_form, default_appliance_form, example_schema)
 from flask_server.test_page import homePage
 
-from transformers import LlavaForConditionalGeneration, LlavaProcessor
 import torchvision.transforms as transforms
 
+# heavyweight model:
+# from transformers import LlavaForConditionalGeneration, LlavaProcessor
+# model_name="mistral-community/pixtral-12b"
+# model_class=LlavaForConditionalGeneration
+# processor_class = LlavaProcessor
 
-model_name="mistral-community/pixtral-12b"
-model_class=LlavaForConditionalGeneration
-processor_class = LlavaProcessor
+# lighter model:
+from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
+model_name = "Qwen/Qwen2-VL-7B-Instruct"
+model_class = Qwen2VLForConditionalGeneration
+processor_class = AutoProcessor
 
 load_dotenv()
 OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434/v1")
