e210920dd009c82a14e596e0714cc0d5b8368b70
Tristan H <tristanskyhook@gmail.com>
2025-07-07 09:30:09 -0700

    re test

--- basic_tests/test.sh
+++ basic_tests/test.sh
@@ -29,32 +29,32 @@ get_noauth "${url}/"
 
 get_noauth "${url}/docs"
 
-# task_id1=$(post_multipart "${auth_token}" "${url}/process/file" "${file1}" "${schema}") || exit 1
+task_id1=$(post_multipart "${auth_token}" "${url}/process/file" "${file1}" "${schema}") || exit 1
 
-# echo ""
-# echo "Task ID for file1: ${task_id1}"
+echo ""
+echo "Task ID for file1: ${task_id1}"
 
-# task_id2=$(post_multipart "${auth_token}" "${url}/process/home" "${file2}" "${schema}") || exit 1
+task_id2=$(post_multipart "${auth_token}" "${url}/process/home" "${file2}" "${schema}") || exit 1
 
-# echo ""
-# echo "Task ID for file2: ${task_id2}"
+echo ""
+echo "Task ID for file2: ${task_id2}"
 
-# task_id3=$(post_multipart "${auth_token}" "${url}/process/appliance" "${file3}" "${schema}") || exit 1
+task_id3=$(post_multipart "${auth_token}" "${url}/process/appliance" "${file3}" "${schema}") || exit 1
 
-# echo ""
-# echo "Task ID for file3: ${task_id3}"
+echo ""
+echo "Task ID for file3: ${task_id3}"
 
-# post_file "${auth_token}" "${url}/process/ocr" "${file1}" "${schema}"
+post_file "${auth_token}" "${url}/process/ocr" "${file1}" "${schema}"
 
-# post_file "${auth_token}" "${url}/process/ocr" "${file2}" "${schema}"
+post_file "${auth_token}" "${url}/process/ocr" "${file2}" "${schema}"
 
-# post_file "${auth_token}" "${url}/process/ocr" "${file3}" "${schema}"
+post_file "${auth_token}" "${url}/process/ocr" "${file3}" "${schema}"
 
-# get "${auth_token}" "${url}/tasks/${task_id1}"
+get "${auth_token}" "${url}/tasks/${task_id1}"
 
-# get "${auth_token}" "${url}/tasks/${task_id2}"
+get "${auth_token}" "${url}/tasks/${task_id2}"
 
-# get "${auth_token}" "${url}/tasks/${task_id3}"]
+get "${auth_token}" "${url}/tasks/${task_id3}"]
 
 get "${auth_token}" "${url}/tasks"
 
