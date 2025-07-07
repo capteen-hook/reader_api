23f8ce171999f28400590dacfa56aad8c6b23521
Tristan H <tristanskyhook@gmail.com>
2025-07-06 22:42:55 -0700

    yer

--- flask_server/celery.py
+++ flask_server/celery.py
@@ -6,7 +6,7 @@ load_dotenv()
 def make_celery(app_name=__name__):
     user = os.getenv('RABBITMQ_DEFAULT_USER', 'user')
     password = os.getenv('RABBITMQ_DEFAULT_PASS', 'password')
-    host = os.getenv('RABBITMQ_HOST', 'localhost')
+    host = os.getenv('RABBITMQ_CONTAINER_NAME', 'rabbitmq')
     port = os.getenv('RABBITMQ_PORT', '5672')
 
     broker_url = f"pyamqp://{user}:{password}@{host}:{port}//"
