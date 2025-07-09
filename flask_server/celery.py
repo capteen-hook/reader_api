09bb59e6f630fcab39af68aaf4273c75461508d8
Tristan H <tristanskyhook@gmail.com>
2025-07-09 14:18:39 -0700

    gotta keep em seperated

--- flask_server/celery.py
+++ flask_server/celery.py
@@ -6,18 +6,13 @@ load_dotenv()
 def make_celery(app_name=__name__):
     user = os.getenv('RABBITMQ_DEFAULT_USER', 'user')
     password = os.getenv('RABBITMQ_DEFAULT_PASS', 'password')
-    
-    host = 'localhost'
-    # host = os.getenv('RABBITMQ_CONTAINER_NAME', 'rabbitmq')
-    
+    host = os.getenv('RABBITMQ_CONTAINER_NAME', 'rabbitmq')
     port = os.getenv('RABBITMQ_PORT', '5672')
     
-    redis_name = 'localhost'
-    # redis_name = os.getenv('REDIS_CONTAINER_NAME', 'redis')
+    redis_name = os.getenv('REDIS_CONTAINER_NAME', 'redis')
     redis_port = os.getenv('REDIS_PORT', '6379')
 
     broker_url = f"pyamqp://{user}:{password}@{host}:{port}//"
     return Celery(app_name, broker=broker_url, backend=f'redis://{redis_name}:{redis_port}/0', include=['flask_server.tasks'])
-
 
 celery = make_celery()
