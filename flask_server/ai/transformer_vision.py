265ce7888199ac2445a8a6a3dec41af1809f876a
Tristan H <tristanskyhook@gmail.com>
2025-07-09 14:35:01 -0700

    performance

--- flask_server/ai/transformer_vision.py
+++ flask_server/ai/transformer_vision.py
@@ -21,6 +21,11 @@ def replace_containerized_path(file_path):
     return file_path
 
 def load_model():
+    print("CUDA available:", torch.cuda.is_available(), file=sys.stderr)
+    print("Supports float16:", torch.cuda.get_device_capability()[0] >= 7)
+    print("CUDA device:", torch.cuda.current_device(), file=sys.stderr)
+    print("CUDA name:", torch.cuda.get_device_name(torch.cuda.current_device()), file=sys.stderr)
+
     if os.getenv("LIGHTWEIGHT_MODEL", "True").lower() in ["true", "1", "yes"]:
         # lighter model:
         from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
@@ -36,7 +41,7 @@ def load_model():
 
     print(f"Using model: {model_name}", file=sys.stderr)
     device = torch.device("cuda")
-    dtype = torch.float32
+    dtype = torch.float16 if torch.cuda.is_available() else torch.float32
     # it will have to download the model, which might take a while.
     model_kwargs={"device_map": "auto", "torch_dtype": dtype}
     processor_kwargs={"device_map": "gpu"}
@@ -134,13 +139,13 @@ def process_vision_multiple(file_path, schema):
         try:
             image = Image.open(imagename)
             
-            # image_tensor = transform(image).to(device=_device, dtype=_dtype)
-            # result = page_summary_generator({"text": prompt, "images": image_tensor})
-            result = page_summary_generator({"text": prompt, "images": [image]})
+            image_tensor = transform(image).to(device=_device, dtype=_dtype)
+            result = page_summary_generator({"text": prompt, "images": image_tensor})
+            #result = page_summary_generator({"text": prompt, "images": [image]})
             results.append(result)
             
             # Free up memory
-            #del image_tensor
+            del image_tensor
             del image
             torch.cuda.empty_cache()
             gc.collect()
@@ -189,7 +194,9 @@ def process_vision(file_path, schema):
         
         print(f"Starting generation for image {file_path}", file=sys.stderr)
         
-        result = image_summary_generator({"text": prompt, "images": [image]})
+        #result = image_summary_generator({"text": prompt, "images": [image]})
+        image_tensor = transform(image).to(device=_device, dtype=_dtype)
+        result = page_summary_generator({"text": prompt, "images": image_tensor})
         
         print(f"Image processed successfully", file=sys.stderr)
         
