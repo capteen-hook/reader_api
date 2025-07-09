c57b413e84aeb9e69ccd5431458bfca0d04e0a7b
Tristan H <tristanskyhook@gmail.com>
2025-07-09 12:29:57 -0700

    yes

--- flask_server/ai/transformer_vision.py
+++ flask_server/ai/transformer_vision.py
@@ -14,17 +14,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if os.getenv("LIGHTWEIGHT_MODEL", "True").lower() in ["true", "1", "yes"]:
-    # heavyweight model:
-    from transformers import LlavaForConditionalGeneration, LlavaProcessor
-    model_name="mistral-community/pixtral-12b"
-    model_class=LlavaForConditionalGeneration
-    processor_class = LlavaProcessor
-else:
     # lighter model:
     from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
     model_name = "Qwen/Qwen2-VL-7B-Instruct"
     model_class = Qwen2VLForConditionalGeneration
     processor_class = AutoProcessor
+else:
+    # heavyweight model:
+    from transformers import LlavaForConditionalGeneration, LlavaProcessor
+    model_name="mistral-community/pixtral-12b"
+    model_class=LlavaForConditionalGeneration
+    processor_class = LlavaProcessor
 
 print(f"Using model: {model_name}", file=sys.stderr)
 device = torch.device("cuda")
