d49c1b0ab72cffd37b5b5faf0259e495d0438860
Tristan Sky Hook <tristanskyhook@gmail.com>
2025-07-17 12:35:16 -0700

    stuff

--- react_server/app/api/chat/route.ts
+++ react_server/app/api/chat/route.ts
@@ -2,26 +2,45 @@ import { ollama } from "ollama-ai-provider";
 import { frontendTools } from "@assistant-ui/react-ai-sdk";
 import { streamText } from "ai";
 import { Experimental_StdioMCPTransport } from 'ai/mcp-stdio';
-import { experimental_createMCPClient as createMCPClient } from "ai";
+import { experimental_createMCPClient as createMCPClient, ToolSet } from "ai";
+import path from "path";
 
 export const runtime = "nodejs";
 
-const transport = new Experimental_StdioMCPTransport({
-  command: "npx",
-  args: [
-    "-y",
-    "@h1deya/mcp-server-weather"
-  ]
-});
+// const transport = new Experimental_StdioMCPTransport({
+//   command: "npx",
+//   args: [
+//     "-y",
+//     "@h1deya/mcp-server-weather"
+//   ]
+// });
 
-const mcpClient = await createMCPClient({
-  transport,
-});
+async function createMCPTools() {
+  const mcpServerPath = path.resolve(
+    "node_modules/.bin/mcp-server-weather" 
+  );
 
-const mcpTools = await mcpClient.tools();
+  const transport = new Experimental_StdioMCPTransport({
+    command: mcpServerPath,
+  });
+
+  const mcpClient = await createMCPClient({ transport });
+
+  return await mcpClient.tools();
+}
+let mcpTools: null | ToolSet = null;
+
+async function getMcpTools() {
+  if (!mcpTools) {
+    mcpTools = await createMCPTools();
+  }
+  return mcpTools;
+}
 
 export async function POST(req: Request) {
   const { messages, system, tools } = await req.json();
+
+  const this_mcpTools = await getMcpTools();
 
   const result = streamText({
     model: ollama("llama3.1:latest"),
@@ -31,7 +50,7 @@ export async function POST(req: Request) {
     system,
     tools: {
       ...frontendTools(tools),
-      ...mcpTools,
+      ...this_mcpTools,
     },
     onError: console.log,
   });
